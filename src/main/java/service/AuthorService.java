9c8ba3f7ade18ff196aad167632cf1344413da25
rababogdan <rababobo@yahoo.com>
2021-03-11 19:34:06 +0200

    update

--- src/main/java/service/AuthorService.java
+++ src/main/java/service/AuthorService.java
@@ -1,4 +1,25 @@
 package service;
 
+import dao.AuthorDao;
+import utils.UtilMethods;
+
+import java.util.Scanner;
+
 public class AuthorService {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    private static final AuthorDao authorDao = new AuthorDao();
+
+    public void createAuthor(){
+        System.out.println("Please insert author's first name:");
+        String firstname = scanner.next() + scanner.nextLine();
+        firstname = UtilMethods.firstLetterToUpperCase(firstname);
+        System.out.println("Please insert veterinarian's last name:");
+        String lastName = scanner.next() + scanner.nextLine();
+        lastName = UtilMethods.firstLetterToUpperCase(lastName);
+        System.out.println("Please insert veterinarian's address:");
+        String address = scanner.next() + scanner.nextLine();
+        System.out.println("Please insert veterinarian's speciality:");
+        String speciality = scanner.next() + scanner.nextLine();
+    }
 }
